100a185f60438ef547a558c997556a8a60de95fb
Smriti Panta <smritipanta1@gmail.com>
2025-05-20 09:18:15 +0545

    platform page done

--- src/app/platform-page/page.tsx
+++ src/app/platform-page/page.tsx
@@ -1,7 +1,94 @@
+"use client";
+import * as React from "react";
+import { BiBookAlt } from "react-icons/bi";
 import HeroSection from "@/components/homepage/hero-section/HeroSection";
 import { Navbar } from "@/components/Navbar";
 import CTASection from "@/components/platformpage/CTASection";
 import { Footer } from "@/components/platformpage/platformFooter";
+import ServicesSection, {
+  Service
+} from "@/components/platformpage/ServicesSection";
+import { FaMicrochip, FaToolbox, FaUsers } from "react-icons/fa";
+import { FaTimeline } from "react-icons/fa6";
+
+const services: Array<Service> = [
+  {
+    title: "Transform your brand",
+    description:
+      "Whether it’s streamlining your operations, connecting with your audience, or enhancing your digital presence, our solutions are designed to help your brand thrive. ",
+    icon: <BiBookAlt />,
+    asset: {
+      src: "/transform.png",
+      alt: ""
+    }
+  },
+  {
+    title: "Free Yourself with AI Integration",
+    description:
+      "Our AI analyzes customer behavior, predicts trends, and personalizes promotions to boost engagement and revenue, streamlining marketing and menu optimization.",
+    icon: <FaUsers />,
+    asset: {
+      src: "/AI.png",
+      alt: ""
+    }
+  },
+  {
+    title: "Mobile App Development",
+    description:
+      "With intuitive features and seamless integration, we help you connect with your customers like never before—making your restaurant stand out in the digital age with our cutting-edge apps.",
+    icon: <FaMicrochip />,
+    asset: {
+      src: "/mobileapp.png",
+      alt: ""
+    }
+  },
+  {
+    title: "Online Delivery Service Partners",
+    description:
+      "Streamline your restaurant’s online orders with our platform, seamlessly integrated with top Delivery Service Providers (DSPs) for effortless ordering and smooth delivery operations.",
+    icon: <FaTimeline />,
+    asset: {
+      src: "/onlineDelivery.png",
+      alt: ""
+    },
+    actions: [{ label: "Learn More", href: "#" }]
+  },
+  {
+    title: "Automated Marketing",
+    description:
+      "By leveraging data-driven insights, our system creates personalized email campaigns, automated social media posts, and targeted promotions to keep your customers engaged.",
+    icon: <FaToolbox />,
+    asset: {
+      src: "/automated.png",
+      alt: ""
+    },
+    points: [],
+    actions: [{ label: "Learn More", href: "#" }]
+  },
+  {
+    title: "Customer Relationship Management",
+    description:
+      "Our platform helps you track customer preferences, manage reservations, and create personalized experiences that keep diners coming back.",
+    icon: <FaUsers />,
+    asset: {
+      src: "/customer.png",
+      alt: ""
+    },
+    actions: [{ label: "Learn More", href: "#" }]
+  },
+
+  {
+    title: "Integrations",
+    description:
+      "Integrate essential tools to streamline operations and elevate customer experience. Connect with POS systems for effortless order and inventory management, and payment gateways for secure transactions.",
+    icon: <FaTimeline />,
+    asset: {
+      src: "/integration.png",
+      alt: ""
+    },
+    actions: [{ label: "Learn More", href: "#" }]
+  }
+];
 
 export default function PlatformPage() {
   return (
@@ -30,6 +117,7 @@ export default function PlatformPage() {
           "#1F4CFF",
           "#6B00B6"
         ]}></HeroSection>
+      <ServicesSection services={services} />
       <CTASection />
       <Footer />
     </section>
---
+++ src/components/platformpage/ServicesSection.tsx
@@ -0,0 +1,246 @@
+import * as React from "react";
+import { twMerge } from "tailwind-merge";
+import { ClassValue } from "tailwind-variants";
+import Link, { type LinkProps } from "next/link";
+
+import { Container } from "@/components/Container";
+import { Button, type ButtonProps } from "@/components/Button";
+import { ANIMATION_DELAY } from "@/lib/siteConfig";
+import Image from "next/image";
+import transform from "@/../public/transform.png";
+
+function getExtension(src: string) {
+  const imageExtensions = [
+    "jpg",
+    "jpeg",
+    "png",
+    "gif",
+    "bmp",
+    "webp",
+    "svg",
+    "tiff",
+    "ico",
+    "avif"
+  ];
+  const videoExtensions = [
+    "mp4",
+    "avi",
+    "mov",
+    "wmv",
+    "flv",
+    "mkv",
+    "webm",
+    "mpeg"
+  ];
+
+  const ext = src.split(".").pop()?.toLowerCase();
+  if (!ext) return "other";
+  if (imageExtensions.includes(ext)) return "image";
+  if (videoExtensions.includes(ext)) return "video";
+
+  return "other";
+}
+
+export type Service = {
+  title: string;
+  description: string;
+  points?: Array<string>;
+  icon?: React.ReactElement;
+  asset: { src: string; alt?: string; [x: string]: unknown };
+  actions?: Array<ButtonProps & LinkProps & { label: string }>;
+};
+
+export type ServicesSectionProps = {
+  services: Array<Service>;
+  classes?: { root?: ClassValue };
+};
+
+export default function ServicesSection({
+  services,
+  classes
+}: ServicesSectionProps) {
+  return (
+    <div>
+      <section
+        id="/services"
+        className={twMerge("py-16 bg-background scroll-mt-16", classes?.root)}>
+        <Container className=" text-center space-y-8">
+          <h2 className="text-4xl lg:text-4xl font-bold lg:tracking-tight">
+            What We Do for You: Simplifying Your Success,
+            <br />
+            One Solution at a Time
+          </h2>
+        </Container>
+        <Container className="bg-muted border mt-16 lg:rounded-lg overflow-x-clip">
+          {services.slice(0, 2).map((service, idx) => (
+            <div
+              key={idx}
+              className="group grid md:grid-cols-2 py-10 gap-x-12 lg:gap-x-16">
+              <div className="grid">
+                <div className="max-w-lg">
+                  <div className="shrink-0 [&_svg]:size-16 text-muted-foreground mt-8 md:mt-4">
+                    {service.icon}
+                  </div>
+
+                  <h3 className="text-2xl font-bold mt-4 text-balance">
+                    {service.title}
+                  </h3>
+
+                  <p className="mt-4 text-foreground/85 text-balance">
+                    {service.description}
+                  </p>
+
+                  {service.points?.length ? (
+                    <ul className="grid mt-6 text-left gap-y-4 list-disc list-inside">
+                      {service.points.map((point, idx) => (
+                        <li
+                          key={idx}
+                          className="text-sm text-foreground/80"
+                          data-aos="fade-in"
+                          data-aos-delay={(idx + 1) * ANIMATION_DELAY}>
+                          {point}
+                        </li>
+                      ))}
+                    </ul>
+                  ) : null}
+                </div>
+              </div>
+
+              <div className="grid place-items-center" data-aos="fade-right">
+                {getExtension(service.asset.src) === "image" ? (
+                  <Image
+                    src={service.asset.src}
+                    alt={service.asset.alt || "Service image"}
+                    width={379}
+                    height={379}
+                    className="rounded-lg"
+                  />
+                ) : null}
+              </div>
+            </div>
+          ))}
+        </Container>
+      </section>
+
+      <section
+        id="/services"
+        className={twMerge("py-16 bg-background scroll-mt-16", classes?.root)}>
+        <Container className=" text-center space-y-8">
+          <h2 className="text-4xl lg:text-4xl font-bold lg:tracking-tight">
+            Your Brand, Your Website
+            <br />
+            Make yourself stand-out in the Competition
+          </h2>
+        </Container>
+        <Container className="bg-muted border mt-16 lg:rounded-lg overflow-x-clip">
+          {services.slice(2, 4).map((service, idx) => (
+            <div
+              key={idx}
+              className="group grid md:grid-cols-2 py-10 gap-x-12 lg:gap-x-16">
+              <div className="grid place-items-center" data-aos="fade-right">
+                {getExtension(service.asset.src) === "image" ? (
+                  <Image
+                    src={service.asset.src}
+                    alt={service.asset.alt || "Service image"}
+                    width={379}
+                    height={379}
+                    className="rounded-lg"
+                  />
+                ) : null}
+              </div>
+              <div className="grid">
+                <div className="max-w-lg">
+                  <div className="shrink-0 [&_svg]:size-16 text-muted-foreground mt-8 md:mt-4">
+                    {service.icon}
+                  </div>
+
+                  <h3 className="text-2xl font-bold mt-4 text-balance">
+                    {service.title}
+                  </h3>
+
+                  <p className="mt-4 text-foreground/85 text-balance">
+                    {service.description}
+                  </p>
+
+                  {service.points?.length ? (
+                    <ul className="grid mt-6 text-left gap-y-4 list-disc list-inside">
+                      {service.points.map((point, idx) => (
+                        <li
+                          key={idx}
+                          className="text-sm text-foreground/80"
+                          data-aos="fade-in"
+                          data-aos-delay={(idx + 1) * ANIMATION_DELAY}>
+                          {point}
+                        </li>
+                      ))}
+                    </ul>
+                  ) : null}
+                </div>
+              </div>
+            </div>
+          ))}
+        </Container>
+      </section>
+
+      <section
+        id="/services"
+        className={twMerge("py-16 bg-background scroll-mt-16", classes?.root)}>
+        <Container className=" text-center space-y-8">
+          <h2 className="text-4xl lg:text-4xl font-bold lg:tracking-tight">
+            AI-Driven Marketing <br />
+            Precision Campaigns at your Fingertips
+          </h2>
+        </Container>
+        <Container className="bg-muted border mt-16 lg:rounded-lg overflow-x-clip">
+          {services.slice(4, 7).map((service, idx) => (
+            <div
+              key={idx}
+              className="group grid md:grid-cols-2 py-10 gap-x-12 lg:gap-x-16">
+              <div className="grid">
+                <div className="max-w-lg">
+                  <div className="shrink-0 [&_svg]:size-16 text-muted-foreground mt-8 md:mt-4">
+                    {service.icon}
+                  </div>
+
+                  <h3 className="text-2xl font-bold mt-4 text-balance">
+                    {service.title}
+                  </h3>
+
+                  <p className="mt-4 text-foreground/85 text-balance">
+                    {service.description}
+                  </p>
+
+                  {service.points?.length ? (
+                    <ul className="grid mt-6 text-left gap-y-4 list-disc list-inside">
+                      {service.points.map((point, idx) => (
+                        <li
+                          key={idx}
+                          className="text-sm text-foreground/80"
+                          data-aos="fade-in"
+                          data-aos-delay={(idx + 1) * ANIMATION_DELAY}>
+                          {point}
+                        </li>
+                      ))}
+                    </ul>
+                  ) : null}
+                </div>
+              </div>
+
+              <div className="grid place-items-center" data-aos="fade-right">
+                {getExtension(service.asset.src) === "image" ? (
+                  <Image
+                    src={service.asset.src}
+                    alt={service.asset.alt || "Service image"}
+                    width={379}
+                    height={379}
+                    className="rounded-lg"
+                  />
+                ) : null}
+              </div>
+            </div>
+          ))}
+        </Container>
+      </section>
+    </div>
+  );
+}
